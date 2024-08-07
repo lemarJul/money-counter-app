5c4b1926fec4ec895a0e09029464ca75bb067f12
Julien Lemarchand <lemar.jul@gmail.com>
2024-08-07 23:45:41 +0200

    feat: Add NumberInput component and integrate with App
    - Integrate NumberInput
    - Style NumberInput using styled-components
    - Update App layout with new Container and Form styles # Please
    enter the commit message for your changes. Lines
    starting # with '#' will be ignored, and an empty message aborts the commit.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,28 +4,17 @@ import "./App.css";
 // State
 import { useCashFloat } from "../state/cashFloat.state.ts";
 // Components
-import { NumberInput } from "./CashInput";
+import { NumberInput } from "./NumberInput";
 import { TotalDisplay } from "./TotalDisplay";
-import { Radio } from "./Radio";
 
 const Container = styled.div`
   height: 100%;
   display: flex;
   flex-direction: column;
+  gap: var(--spacing-m);
   padding: 1rem;
   position: relative;
   font-size: 2rem;
-`;
-const Top = styled.div`
-  display: flex;
-  place-item: center;
-
-  min-height: 25%;
-  max-height: 25%;
-  padding: 1rem;
-  gap: 1rem;
-  background-color: var(--color-dark);
-  color: white;
 `;
 
 const Form = styled.form`
@@ -35,7 +24,7 @@ const Form = styled.form`
   height: 100%;
   width: 100%;
   overflow: scroll;
-  border: 2px solid gray;
+  border: 2px solid var(--color-dark);
   border-radius: var(--border-radius);
 `;
 
@@ -44,43 +33,50 @@ const Row = styled.div`
   display: flex;
   flex-direction: row;
   width: 100%;
-  height: 100%;
+  height: 100px;
   justify-content: space-between;
   align-items: center;
   border-bottom: 1px solid var(--color-dark);
-  margin: var(--spacing-xs) 0;
   padding: var(--spacing-m);
-  gap: var(--spacing-s);
+  gap: var(--spacing-m);
 `;
-// const Header = styled.div`
-//   z-index: 1;
-//   position: sticky;
-//   top: 0;
-//   color: white;
-//   background-color: var(--color-dark);
-//   display: flex;
-//   height: fit-content;
-//   justify-content: space-between;
-//   align-items: center;
-//   border-bottom: 2px solid var(--color-light);
-//   padding: var(--spacing-m);
-//   gap: var(--spacing-s);
-//   font-size: var(--font-size-l);
-// `;
 
 const RowLabel = styled.span`
-  width: 100%;
-  padding: var(--spacing-m);
-`;
-
-const RadioContainer = styled.div`
   display: flex;
   flex-direction: column;
-  gap: var(--spacing-s);
-  justify-content: center;
-  width: 100%;
-  color: white;
+  text-align: left;
+  font-size: var(--font-size-xl);
+  min-width: 80px;
 `;
+
+const Header = styled.div`
+  z-index: 1;
+  position: sticky;
+  top: 0;
+  color: white;
+  background-color: var(--color-dark);
+  display: flex;
+  height: fit-content;
+  justify-content: space-between;
+  align-items: center;
+
+  padding: var(--spacing-m);
+  gap: var(--spacing-s);
+  font-size: var(--font-size-l);
+
+  & > span {
+    min-width: 80px;
+  }
+`;
+
+// const RadioContainer = styled.div`
+//   display: flex;
+//   // flex-direction: column;
+//   gap: var(--spacing-s);
+//   justify-content: center;
+//   width: 100%;
+//   color: white;
+// `;
 
 function App() {
   const { cashFloat, CashFloatTotalValue, setCounter, resetCashFloat } =
@@ -118,47 +114,63 @@ function App() {
       {/* <span>
         <button onClick={resetCashFloat}>Reset</button>
         <button onClick={() => setActiveCounter(null)}>total</button>
-      </span>
+      </span> */}
       <Form action="">
-        {/* <Header>
+        <Header>
           <RowLabel>€</RowLabel>
-          <RowLabel>unit</RowLabel>
-          <RowLabel>roll</RowLabel>
-          <RowLabel>gram</RowLabel>
-        </Header> */}
+          <span>units</span>
+          <span>rolls</span>
+          <span>grams</span>
+        </Header>
         {cashFloat.map((inventory, inventoryIndex) => (
           <Row key={inventoryIndex}>
             <RowLabel>
-              {inventory.label}€
-              <div style={{ width: "100%", fontSize: "var(--font-size-m)" }}>
+              <span>{inventory.label}</span>
+              <span style={{ width: "100%", fontSize: "var(--font-size-m)" }}>
                 x {inventory.totalUnits}
-              </div>
+              </span>
             </RowLabel>
-            <RadioContainer>
-              {Object.entries(inventory.counters).map(([key, counter]) => {
-                const typedKey = key as keyof typeof inventory.counters;
-                return (
-                  counter && (
-                    <Radio
-                      key={inventoryIndex + key}
-                      id={inventoryIndex + key}
-                      label={counter?.count + " " + counter?.unit}
-                      value={counter?.count}
-                      checked={
-                        activeCounter?.inventoryIndex === inventoryIndex &&
-                        activeCounter.counterKey === typedKey
-                      }
-                      onChange={() =>
-                        setActiveCounter({
-                          inventoryIndex: inventoryIndex,
-                          counterKey: typedKey,
-                        })
-                      }
-                    />
-                  )
-                );
-              })}
-            </RadioContainer>
+
+            {/* {Object.entries(inventory.counters).map(([key, counter]) => {
+              const typedKey = key as keyof typeof inventory.counters;
+              return (
+                counter && (
+                  <Radio
+                    key={inventoryIndex + key}
+                    id={inventoryIndex + key}
+                    label={counter?.count.toString()}
+                    // label={counter?.count + " " + counter?.unit}
+                    value={counter?.count}
+                    checked={
+                      activeCounter?.inventoryIndex === inventoryIndex &&
+                      activeCounter.counterKey === typedKey
+                    }
+                    onChange={() =>
+                      setActiveCounter({
+                        inventoryIndex: inventoryIndex,
+                        counterKey: typedKey,
+                      })
+                    }
+                  />
+                )
+              );
+            })} */}
+
+            {Object.entries(inventory.counters).map(([key, counter]) => {
+              const typedKey = key as keyof typeof inventory.counters;
+              return (
+                counter && (
+                  <NumberInput
+                    key={inventoryIndex + key}
+                    // label={counter?.count + " " + counter?.unit}
+                    value={counter?.count}
+                    onChange={(val: number) =>
+                      setCounter(inventoryIndex, typedKey, val)
+                    }
+                  />
+                )
+              );
+            })}
           </Row>
         ))}
       </Form>
